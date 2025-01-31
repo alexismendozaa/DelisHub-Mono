cd85804817cebdcb8a0db236561937ee927b6b43
Johana Elizalde <njelizalde@uce.edu.ec>
2025-01-30 20:57:02 -0500

    feat(api): ✨ update base URL for API client to new server address

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+baseURL: 'http://3.221.112.57:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
